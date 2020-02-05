f54b51436ca86ec6891af310fed286c8395383eb
Rodrigo Montes <rjmontes@gmail.com>
2020-02-05 13:28:28 -0300

    Video 81 - SpaceShip and Music
    - Added Splash Screen Scene
    - Added space ship
    - Added Music (non stop music from splash screen into the game with DontDestroyOnLoad(this.gameObject) )

---
+++ Assets/Scripts/Splash.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Splash : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 10f;
+
+    // Called before Start()
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);     // this object is not destroyed when loading the next scene
+    }
+
+        // Start is called before the first frame update
+        void Start()
+    {
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex >= SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;         // go back to the splash screen
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
