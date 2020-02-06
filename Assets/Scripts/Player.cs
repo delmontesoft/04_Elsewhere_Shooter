d1e5aac4327008c717dd07347b735ddec147de68
Rodrigo Montes <rjmontes@gmail.com>
2020-02-06 17:44:28 -0300

    Video 89 - Pitch, Yaw, Roll
    - Added pitch, yaw and roll effect when moving the ship across the screen (factors are tunneable in the editor)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] [Tooltip("In ms^-1")] float movementSpeed = 20f;
-    [SerializeField] [Tooltip("In m")] float xRange = 6f;
-    [SerializeField] [Tooltip("In m")] float yRange = 6f;
+    [SerializeField] [Tooltip("In ms^-1")] float movementSpeed = 25f;
+    [SerializeField] [Tooltip("In m")] float xRange = 12f;      //16:9 screen
+    [SerializeField] [Tooltip("In m")] float yRange = 8f;       //16:9 screen
+
+    [SerializeField] float positionPitchFactor = -1.25f;         //16:9 screen
+    [SerializeField] float controlPitchFactor = -20f;            //16:9 screen
+
+    [SerializeField] float positionYawFactor = 1.5f;            //16:9 screen
+
+    [SerializeField] float controlRollFactor = -20f;            //16:9 screen
+
+    float xThrow, yThrow;
 
     // Start is called before the first frame update
     void Start()
@@ -18,29 +28,37 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        RespondToHorizontalInput();
-        RespondToVerticalInput();
+        ProcessTranslation();
+        ProcessRotation();
     }
 
-    private void RespondToHorizontalInput()
+    private void ProcessTranslation()
     {
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
         float xOffset = xThrow * movementSpeed * Time.deltaTime;
-
-        float xRawPos = transform.localPosition.x + xOffset;
-        float xClampedPos = Mathf.Clamp(xRawPos, -xRange, xRange);
-
-        transform.localPosition = new Vector3(xClampedPos, transform.localPosition.y, transform.localPosition.z);
-    }
-
-    private void RespondToVerticalInput()
-    {
-        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
         float yOffset = yThrow * movementSpeed * Time.deltaTime;
 
+        float xRawPos = transform.localPosition.x + xOffset;
         float yRawPos = transform.localPosition.y + yOffset;
+
+        float xClampedPos = Mathf.Clamp(xRawPos, -xRange, xRange);
         float yClampedPos = Mathf.Clamp(yRawPos, -yRange, yRange);
 
-        transform.localPosition = new Vector3(transform.localPosition.x, yClampedPos, transform.localPosition.z);
+        transform.localPosition = new Vector3(xClampedPos, yClampedPos, transform.localPosition.z);
+    }
+
+    private void ProcessRotation()
+    {
+        float positionPitch = transform.localPosition.y * positionPitchFactor;
+        float controlPitch = yThrow * controlPitchFactor;
+        float pitch =  positionPitch + controlPitch;
+
+        float yaw = transform.localPosition.x * positionYawFactor;
+
+        float roll = xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 }
