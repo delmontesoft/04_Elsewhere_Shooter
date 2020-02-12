20f5ecb9ea6b71f3d7a0200adf36a0523ed123bd
Rodrigo Montes <rjmontes@gmail.com>
2020-02-12 20:33:52 -0300

    Video 101 - Player Explosion
    - Enable player explosion as an FX when hitting stuff
    - Reload level when hitting stuff
    - Managed music persistence and multiple instances of music player

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] [Tooltip("In seconds")] float levelLoadDelay = 1f;
+    [SerializeField] [Tooltip("FX prefab on player")] GameObject deathFX;
+
     private void OnTriggerEnter(Collider other)
     {
         StartDeathSequence();
@@ -12,5 +16,12 @@ public class CollisionHandler : MonoBehaviour
     private void StartDeathSequence()
     {
         SendMessage("OnPlayerDeath");
+        deathFX.SetActive(true);
+        Invoke("ReloadScene", levelLoadDelay);
+    }
+
+    private void ReloadScene()      //string referenced
+    {
+        SceneManager.LoadScene(1);
     }
 }
--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -5,7 +5,15 @@ public class MusicPlayer : MonoBehaviour
     // Called before Start()
     void Awake()
     {
-        DontDestroyOnLoad(gameObject);     // this object is not destroyed when loading the next scene
+        int numMusicPlayers = FindObjectsOfType<MusicPlayer>().Length;
+        
+        if (numMusicPlayers > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);     // this object is not destroyed when loading the next scene
+        }
     }
-
 }
