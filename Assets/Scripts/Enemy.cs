4d6da89bcdf93c3172e70c0893df437c4eb8267b
Rodrigo Montes <rjmontes@gmail.com>
2020-02-14 20:14:37 -0300

    Video 103 - Destroy enemies!
    - Added a script to destroy enemies on hit (Enemy.cs)
    - Added Collision to lasers particle system (and made a prefab for them)
    - Adjusted firing speed (and other stuff) for  lazors
    - Removed Box Colliders from all enemy ships (and prefabs) and added dynamically when game starts

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AddNonTriggerBoxCollider();
+    }
+
+    private void AddNonTriggerBoxCollider()
+    {
+        Collider addedBoxCollider = gameObject.AddComponent<BoxCollider>();
+        addedBoxCollider.isTrigger = false;
+        addedBoxCollider.enabled = true;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        print("BOOM!");
+        Destroy(gameObject);
+    }
+}
