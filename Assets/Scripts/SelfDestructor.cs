74d720a3f442847ee78832e63a018aaa6dbd9ce7
Rodrigo Montes <rjmontes@gmail.com>
2020-02-17 13:15:37 -0300

    Video 105 - More Explosions!
    - Added explosion to enemies, the explosions are instantiated at run time within the enemy.cs script
    - Added Self destruction script to the explosion FX prefab (with serialized timer)
    - Added simple score UI (just a non functional placeholder for now)
    - Added Venus Rising Font for the score UI

---
+++ Assets/Scripts/SelfDestructor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestructor : MonoBehaviour
+{
+    [SerializeField] float time = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, time);
+    }
+}
