2be3731e99ea39b239f851d2548724422d3987f6
Rodrigo Montes <rjmontes@gmail.com>
2020-02-17 20:35:15 -0300

    Video 108 - Score
    - Added a class to control scoreboard (scoreboard.cs) 
    - Called the method scoreHit from enemies
    - Added a serialized field to enemies to give them score when killed (that is being passed to the method on the scoreboard)

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,14 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 10;
+
+    ScoreBoard scoreBoard;
 
     // Start is called before the first frame update
     void Start()
     {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
         AddNonTriggerBoxCollider();
     }
 
@@ -23,9 +28,10 @@ public class Enemy : MonoBehaviour
 
     void OnParticleCollision(GameObject other)
     {
+        scoreBoard.scoreHit(scorePerHit);
+
         GameObject deathFXInstance = Instantiate(deathFX, transform.position, Quaternion.identity);
         deathFXInstance.transform.parent = parent;
-        Destroy(deathFXInstance, 5f);
 
         Destroy(gameObject);
     }
